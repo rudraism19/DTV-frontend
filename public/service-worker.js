aa69c169c397a04f6630767e83260033c803279a
Kumar <kar98kbi@gmail.com>
2026-06-18 13:49:37 +0530

    Fix toast popup fully hiding off screen

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-547a829cc2dec5f8'; // Updated to bust cache
+const CACHE_NAME = 'app-cache-7cd6694837e92961'; // Updated to bust cache
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();

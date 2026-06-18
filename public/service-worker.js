0c61883db2d038142e237b6def35bf485d4ccacc
Kumar <kar98kbi@gmail.com>
2026-06-18 13:52:11 +0530

    Remove empty CSS ruleset warning

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-7cd6694837e92961'; // Updated to bust cache
+const CACHE_NAME = 'app-cache-6ec607f13138948d'; // Updated to bust cache
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();

231c16398d5fdc367763682e3a26e03b3d27c409
Kumar <kar98kbi@gmail.com>
2026-06-18 14:29:48 +0530

    Fix bug where new careers were not appended properly

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-22ba5a7f47c4bfe8'; // Updated to bust cache
+const CACHE_NAME = 'app-cache-123233b2cd280b6c'; // Updated to bust cache
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();

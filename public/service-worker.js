d58e4df11ffaf9ffe47e037347ef521e6ea6bf59
Kumar <kar98kbi@gmail.com>
2026-06-18 14:09:26 +0530

    Fix background-clip standard property warning

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-1422602ed7f24224'; // Updated to bust cache
+const CACHE_NAME = 'app-cache-cf4cffeb23c045be'; // Updated to bust cache
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();

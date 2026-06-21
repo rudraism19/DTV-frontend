2e88faaa1bfa80e37cc12a2817f0f0450500faa4
Kumar <kar98kbi@gmail.com>
2026-06-22 04:20:23 +0530

    Auto-backfill linkCode on server start

---
+++ backfill.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const db = require('./src/db');
+const crypto = require('crypto');
+
+async function run() {
+  try {
+    const res = await db.query("SELECT id FROM users WHERE role = 'student' AND link_code IS NULL");
+    let c = 0;
+    for (let r of res.rows) {
+      const code = crypto.randomBytes(3).toString('hex').toUpperCase();
+      await db.query("UPDATE users SET link_code = $1 WHERE id = $2", [code, r.id]);
+      c++;
+    }
+    console.log('Updated ' + c + ' students');
+  } catch (err) {
+    console.error(err);
+  } finally {
+    process.exit(0);
+  }
+}
+
+run();

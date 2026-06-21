9cfaf96d1655060cafae3e02c9e294cd050f5333
Kumar <kar98kbi@gmail.com>
2026-06-22 04:33:22 +0530

    Fix backfill to convert ALL users to students

--- backfill.js
+++ backfill.js
@@ -4,11 +4,11 @@ const crypto = require('crypto');
 
 async function run() {
   try {
-    const res = await db.query("SELECT id FROM users WHERE role = 'student' AND link_code IS NULL");
+    const res = await db.query("SELECT id FROM users WHERE link_code IS NULL");
     let c = 0;
     for (let r of res.rows) {
       const code = crypto.randomBytes(3).toString('hex').toUpperCase();
-      await db.query("UPDATE users SET link_code = $1 WHERE id = $2", [code, r.id]);
+      await db.query("UPDATE users SET link_code = $1, role = 'student' WHERE id = $2", [code, r.id]);
       c++;
     }
     console.log('Updated ' + c + ' students');
